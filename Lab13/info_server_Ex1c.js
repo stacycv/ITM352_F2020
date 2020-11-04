46f957b6a8d440a9520a7c2e83df4c5fa9deab19
USERNAME <EMAIL>
2020-11-04 13:12:56 -1000

    updates
    lab 13, assignment 1 & invoice 4

--- Lab13/info_server_Ex1c.js
+++ Lab13/info_server_Ex1c.js
@@ -5,7 +5,7 @@ app.use(express.static('./public'));
 
 app.all('*', function (request, response, next) {
     response.send(request.method + ' to path ' + request.path);
-    next(); 
+    next();
 });
 
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here
--- Lab13/simpleserver2.js
+++ Lab13/simpleserver2.js
@@ -1,12 +1,14 @@
 var http = require('http');
 
-//create a server object:
-http.createServer(function (req, res) {
-    console.log(req.headers); //output the request headers to the console
+function serverCallback (req, res) {
+    console.log(req); //output the request headers to the console
     res.writeHead(200, { 'Content-Type': 'text/html' }); // set MIME type to HTML 
     res.write(`<h1>The server date is: ${Date.now()}</h1>`); //send a response to the client
     res.write('<h1>The client date is: <script>document.write( Date.now() );</script></h1>'); // send another response
     res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+}
+
+//create a server object:
+http.createServer(serverCallback).listen(8080); //the server object listens on port 8080
 
 console.log('Hello world HTTP server listening on localhost port 8080');
