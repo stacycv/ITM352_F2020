91ee9fd53c706a13b5338f6199b5aef33bebd9df
USERNAME <EMAIL>
2020-11-08 07:12:57 -1000

    copy updates

--- Assign1/server.js
+++ Assign1/server.js
@@ -1,11 +1,12 @@
 // referenced from assignment 1 scrreencast and lab 13
 var data = require('./public/products_data.js'); //must have data from product_data.js
 var products = data.products;
+const queryString = ('query-string'); // so it'll load querystring
 var fs = require('fs'); // In lab 13, not sure if needed
 var express = require('express'); //server requires express to run
 var app = express(); // run the express function and start express
 var myParser = require("body-parser");
-const queryString = ('query-string'); // so it'll load querystring
+
 
 // if valid redirect to invoice, if not go order page
 app.all('*', function(request, response, next) {
@@ -25,7 +26,7 @@ app.post("/process_purchase", function(request, response) {
         for (i = 0; i < products.length; i++) {
             qty = POST[`quantity${i}`];
             validQuantities = hasQuantities || qty > 0; // allowed if > than 0
-            validQuantities = validQuantities && isNonNegInt(amount);
+            validQuantities = validQuantities && isNonNegInt(qty);
         }
         // if all quantities are valid go to invoice
         const stringified = queryString.stringify(POST);
@@ -35,12 +36,12 @@ app.post("/process_purchase", function(request, response) {
     }
 });
 //lab 13 reference
-function isNonNegInt(amount, returnErrors = false) { //changed name to have more meaning
+function isNonNegInt(qty, returnErrors = false) { //changed name to have more meaning
     errors = []; // assume no errors at first
-    if (amount == "") { amount = 0; }
-    if (Number(amount) != amount) errors.push('Not a number!'); // Check if string is a number value
-    if (amount < 0) errors.push('Negative value!'); // Check if it is non-negative
-    if (parseInt(amount) != amount) errors.push('Not an integer!'); // Check that it is an integer
+    if (qty == "") { qty = 0; }
+    if (Number(qty) != qty) errors.push('Not a number!'); // Check if string is a number value
+    if (qty < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if (parseInt(qty) != qty) errors.push('Not an integer!'); // Check that it is an integer
     return returnErrors ? errors : (errors.length == 0);
 }
 app.use(express.static('./public')); // makes a static server using express from the public
