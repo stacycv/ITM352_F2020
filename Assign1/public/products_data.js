8010b2d7f46aa11a77e1671f7a95c98b12543660
USERNAME <EMAIL>
2020-11-08 00:14:50 -1000

    invoice and layout updates

--- Assign1/public/products_data.js
+++ Assign1/public/products_data.js
@@ -2,35 +2,35 @@ var products = [
     //P1
     {
         "name": "Back Massage",
-        "time": "60 minute voucher",
+        "time": "60 min",
         "price": 75.00,
         "image": "./images/backMassageA1.jpg"
     },
     //P2
     {
         "name": "Couples Massage",
-        "time": "90 minute voucher",
+        "time": "90 min",
         "price": 125.00,
         "image": "./images/couplesMassageA1.jpg"
     },
     //P3
     {
         "name": "Back Massage",
-        "time": "60 minute voucher",
+        "time": "60 min",
         "price": 125.00,
         "image": "./images/facialSpaA1.jpg"
     },
     //P4
     {
         "name": "Thai Stone Massage",
-        "time": "70 minute voucher",
+        "time": "70 min",
         "price": 75.00,
         "image": "./images/thaiStoneMassage.jpg"
     },
     //P5
     {
         "name": "Thai Foot Massage",
-        "time": "60 minute voucher",
+        "time": "60 min",
         "price": 65.00,
         "image": "./images/thaiFootMassageA1.jpg"
     }
