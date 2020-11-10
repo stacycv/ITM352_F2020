940dbb4870adc0ab1d7131e1bb468cb0b168b8e8
USERNAME <EMAIL>
2020-11-09 23:55:31 -1000

    hello

--- Vasquez_Stacy_Assignment1/public/products_data.js
+++ Vasquez_Stacy_Assignment1/public/products_data.js
@@ -1,38 +1,33 @@
 var products = [
     //P1
     {
-        "name": "Back Massage Chair",
-        "time": "60 min",
+        "name": "Roller",
         "price": 75.00,
         "image": "./images/backMassageA1.jpg"
     },
     //P2
     {
-        "name": "Couples Massage",
-        "time": "90 min",
+        "name": "Balms",
         "price": 125.00,
-        "image": "./images/couplesMassageA1.jpg"
+        "image": "./images/massageBalmsA1.jpg"
     },
     //P3
     {
-        "name": "Back Massage",
-        "time": "60 min",
+        "name": "Candles",
         "price": 125.00,
-        "image": "./images/facialSpaA1.jpg"
+        "image": "./images/massageCandle.jpg"
     },
     //P4
     {
-        "name": "Thai Stone Massage",
-        "time": "70 min",
+        "name": "Oil",
         "price": 75.00,
-        "image": "./images/thaiStoneMassage.jpg"
+        "image": "./images/massageOilA1.jpg"
     },
     //P5
     {
-        "name": "Thai Foot Massage",
-        "time": "60 min",
+        "name": "Hot Stones",
         "price": 65.00,
-        "image": "./images/thaiFootMassageA1.jpg"
+        "image": "./images/massageStoneA1.jpg"
     },
     // all images included are findings from random google searches
     // array was used from Assign 1 page that said to reference SPP3
