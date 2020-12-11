e764d5936d6eaa87a616d5204a3f47a8a443cdb5
USERNAME <EMAIL>
2020-12-10 15:40:37 -1000

    logout login

---
+++ A3 workshop/public/functions.js
@@ -0,0 +1,40 @@
+// This function asks the server for a "service" and converts the response to text. 
+function loadJSON(service, callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('POST', service, false);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+// This function makes a navigation bar from a products_data object
+
+function nav_bar(this_product_key, products_data) {
+    // This makes a navigation bar to other product pages
+    for (this_products_key in products_data) {
+        if (this_products_key == this_product_key) continue;
+        document.write(`<a href='./display_products.html?this_products_key=${this_products_key}'>${this_products_key}<a>&nbsp&nbsp&nbsp;`); //creaating a bunch of hyperlinks 
+    }
+}
+
+//reference https://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
---
+++ A3 workshop/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+var products_data = require('./products.json'); //loading in products data--
+
+
+app.post("/get_products_data", function(request, response) {
+    response.json(products_data);
+});
+
+app.get("/login", function(request, response) {
+    response.cookie('username', 'stacycv', { maxAge: 60 * 10000 }).send('cookie set'); //sets name
+});
+
+app.get("/logout", function(request, response) {
+    username = request.cookies.username; //if you have a username cookie
+    //refernece https://flaviocopes.com/express-cookies/ 
+    response.clearCookie('username').send(`logged out! ${username}`);
+});
+
+app.use(express.static('./public'));
+app.listen(8080, () => console.log(`listening on port 8080`));
