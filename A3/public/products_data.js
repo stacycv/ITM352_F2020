6dcb2953bed856f1ebe69311a1c6b00bb3d58266
USERNAME <EMAIL>
2020-12-11 16:21:07 -1000

    product pages

--- A3/public/products_data.js
+++ A3/public/products_data.js
@@ -92,10 +92,10 @@ The purpose of the products data is to list the price, images and names of the p
     ]
 }
 var products = {
-    "spring2020": spring2020,
-    "summer2020": summer2020,
-    "fall2020": fall2020,
-    "resort2021": resort2021
+    "Spring 2020": spring2020,
+    "Summer 2020": summer2020,
+    "Fall 2020": fall2020,
+    "Resort 2021": resort2021
 }
 
 
